3b196ce37228cf298f34043738252857b3b986bb
mazin <malkthere@gmail.com>
2026-02-01 20:26:02 +0300

    Mini Project Upgrade: Use Templates (Real Web Page)

--- students/views.py
+++ students/views.py
@@ -1,3 +1,3 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 def home(request):
-    return HttpResponse("Welcome to the School Portal!")
+    return render(request, 'students/home.html')
