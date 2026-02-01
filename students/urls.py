e4ca4e130c2c116693998defe5706d5ae9c0ed7e
mazin <malkthere@gmail.com>
2026-02-01 20:30:02 +0300

    Mini Project Feature: Dynamic URL (Student Details)

--- students/urls.py
+++ students/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='students_home'),
+    path('<int:student_id>/', views.student_detail, name='student_detail'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 def home(request):
     return render(request, 'students/home.html')
+
+def student_detail(request, student_id):
+    return HttpResponse(f"Student Profile Page - ID: {student_id}")
