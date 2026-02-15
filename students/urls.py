daee25e238520cdf97c62aafdc8443faab562253
mazin <malkthere@gmail.com>
2026-02-15 06:26:10 +0300

    Class 3 – Templates Deep Dive & Static Files

--- students/urls.py
+++ students/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='students_home'),
+    path('', views.home, name='students-home'),
+    path('about/', views.about, name='students-about'),
+    path('contactus/', views.contactus, name='contactus'),
+
     path('<int:student_id>/', views.student_detail, name='student_detail'),
 ]
--- students/views.py
+++ students/views.py
@@ -2,7 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'students/home.html')
+    context = {
+        'title': 'Home Page',
+        'message': 'Welcome to the Students Application'
+    }
+    return render(request, 'students/home.html', context)
+
+def about(request):
+    context = {
+        'title': 'About Page',
+        'description': 'This application is built using Django.'
+    }
+    return render(request, 'students/about.html', context)
+
+def contactus(request):
+    context = {
+        'title': 'Contact Page',
+        'description': 'developed by Dr. Mazin Alkathiti. E-mail: malkthere@seiyunu.edu.ye'
+
+    }
+    return render(request, 'students/contactus.html', context)
+
+
 
 def student_detail(request, student_id):
     return HttpResponse(f"Student Profile Page - ID: {student_id}")
